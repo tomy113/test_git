70bcf6517df1f8f2adab5e040bb27c4be4fd9d11
dev_huangshaodong <huangshaodong@coohua.com>
2017-09-15 15:45:04 +0800

    init

---
+++ main.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: 
+	> Mail: 
+	> Created Time: Fri Sep 15 15:43:33 2017
+ ************************************************************************/
+
+#include<stdio.h>
+
+
+int main (int * argc , const char *argv) {
+    printf("hello world!\n");
+    return 0;
+}
